10a9c9cdaf7e5f178a1c0a5b61cfe3b3df2a1396
ViltsuV <9432001+ViltsuV@users.noreply.github.com>
2023-05-20 15:20:15 +0300

    add gravatar avatar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,15 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script lang="ts">
+	import { Avatar } from '@skeletonlabs/skeleton'
+</script>
 
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-1 text-center">
-		<h2 class="font-bold">My Introduction</h2>
+		<Avatar
+			src="https://gravatar.com/avatar/38f2aba6683a7e54f4d81252718a17f7?s=200"
+			width="w-32"
+			rounded="rounded-full"
+		/>
+		<h2 class="font-bold">ViltsuV</h2>
 	</div>
 </div>
 
