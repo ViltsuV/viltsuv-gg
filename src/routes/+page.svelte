fccf08dcf032cc1490844c3c8fc533124ffa1ff9
ViltsuV <9432001+ViltsuV@users.noreply.github.com>
2023-05-21 12:32:29 +0300

    align avatar and texts horizontally

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,13 +3,16 @@
 </script>
 
 <div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-1 text-center">
+	<div class="space-y-1 text-center flex flex-row">
 		<Avatar
 			src="https://gravatar.com/avatar/38f2aba6683a7e54f4d81252718a17f7?s=200"
 			width="w-32"
 			rounded="rounded-full"
 		/>
-		<h2 class="font-bold">ViltsuV</h2>
+		<div class="flex flex-col justify-center">
+			<h2 class="font-bold">ViltsuV</h2>
+			<p class="pl-2">info about me</p>
+		</div>
 	</div>
 </div>
 
