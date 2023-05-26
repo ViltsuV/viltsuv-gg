6ce6e643ca39bd20947c5e02de9554c4e39eb133
ViltsuV <9432001+ViltsuV@users.noreply.github.com>
2023-05-26 12:02:36 +0300

    overhaul types to accommodate design changes, add descriptions and examples

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,9 +1,55 @@
 export type Project = {
   name: string
+  /**
+   * @description who is the project's owner?
+   */
+  owner: string
   url: string
   screenshot_url: string
   screenshot_alt: string
+  /**
+   * @description a description of 
+   * - what the project is
+   * - what is/was your involvement level?
+   */
   description: string
+  /**
+   * @description the url slug to the project's page in lower case kebab-case
+   * @example "url-slug-in-kebab-case"
+   */
+  slug: string
+  activity_periods: ActivityPeriod[]
+  /**
+   * @description if you joined the project while it had already started, when did you join?
+   */
+  join_time: string
+  technologies: Technology[]
+  /**
+   * @description if the project was made for a company, which company?
+   */
+  company_name?: string | undefined
+}
+
+export type ActivityPeriod = {
+  /**
+   * @description year-month-day
+   * @example 2023-05-01
+   */
+  start_time: string
+  /**
+   * @description year-month-day
+   * @example 2023-05-01
+   */
+  end_time?: string | undefined
+}
+
+export type Technology = {
+  type: 'frontend' | 'backend' | 'db' | 'api' | 'service'
+  name: string
+  /**
+   * @description a link to the technology's home page
+   */
+  url?: string
 }
 
 export type Projects = Project[]
