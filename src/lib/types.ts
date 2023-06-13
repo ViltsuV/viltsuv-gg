82236d8b4d685f50a15648818100d7dab6953d46
ViltsuV <9432001+ViltsuV@users.noreply.github.com>
2023-06-13 12:31:28 +0300

    add ffmpeg related types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -53,3 +53,63 @@ export type Technology = {
 }
 
 export type Projects = Project[]
+
+// FFMPEG-Command-Generator
+
+export type FFMPEG_Input = {
+  options: string,
+  path: string
+}
+
+export type FFMPEG_Output = {
+  options: string,
+  path: string
+}
+
+export type SupportedVideoEncoders = 
+'copy' |
+'libx264' | 
+'libx265' | 
+'libaom-av1' |
+'librav1e' |
+'libsvtav1' |
+'av1_nvenc' |
+'av1_qsv' |
+'av1_amf' |
+'libvpx' |
+'libvpx-vp9'
+
+export type SupportedAudioEncoders = 
+'copy' |
+'libvo_aacenc' |
+'libmp3lame' |
+'opus' |
+'wav?'
+
+export type SupportedAudioContainers = 'm4a' | 'mp3' | 'wav' | 'opus'
+
+export type SupportedVideoContainers = 'mp4' | 'mkv' | 'webm'
+
+export type SupportedRateControlModes = 'crf' | 'abr'
+
+// https://en.wikipedia.org/wiki/Comparison_of_video_container_formats#Video_coding_formats_support
+export type ContainerSupportedCodecs = {
+  // audio
+  m4a: ['aac'],
+  mp3: ['mp3'],
+  wav: ['pcm'],
+  opus: ['opus'],
+  // video
+  mp4: {
+    audio: ['aac', 'opus'],
+    video: ['av1', 'h.264', 'h.265', 'vp8', 'vp9'],
+  },
+  mkv: {
+    audio: ['aac', 'mp3', 'pcm', 'opus'],
+    video: ['av1', 'h.264', 'h.265', 'vp8', 'vp9'],
+  },
+  webm: {
+    audio: ['opus'],
+    video: ['vp8', 'vp9', 'av1'],
+  }
+}
