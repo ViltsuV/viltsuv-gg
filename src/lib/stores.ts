99d87c8227e9d211d747a61679203750ca5e860f
ViltsuV <9432001+ViltsuV@users.noreply.github.com>
2023-06-15 15:15:26 +0300

    update options store to align with the type updates

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,6 +1,45 @@
-import { writable } from "svelte/store"
+import type { FFMPEG_Options } from "$lib/types"
+import { writable, type Writable } from "svelte/store"
 
-export let global_options = writable('[global options]')
+let options: FFMPEG_Options = {
+  global: {
+    use_global_options: false,
+    overwriting: "ask",
+    stats: false,
+    advanced: {
+      hide_banner: false
+    },
+  },
+  per_file_main: {
+    c: null,
+    f: null,
+    metadata: null,
+    ss: null,
+    sseof: null,
+    t: null,
+    timestamp: null,
+    to: null,
+    advanced: {}
+  },
+  audio: {
+    an: false,
+    ar: null,
+    advanced: {},
+  },
+  video: {
+    vn: false,
+    r: null,
+    s: null,
+    advanced: {
+      hwaccel: null,
+      hwaccel_device: null,
+      hwaccel_output_format: null,
+    },
+  },
+  subtitle: {},
+}
+
+export let ffmpeg_options: Writable<FFMPEG_Options> = writable(options)
 
 export let inputs = writable([{
   options: '[input options]',
