606e21f5ecf7f932746ef9c2c7b7f2e33930209f
ViltsuV <9432001+ViltsuV@users.noreply.github.com>
2023-07-09 23:19:08 +0300

    move input start second eof (-sseof) related code to separate component

--- src/lib/components/PerFileInputOptions.svelte
+++ src/lib/components/PerFileInputOptions.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import Expandable from "$lib/components/Expandable.svelte"
 	import PerFileInputOptions_t from "$lib/components/PerFileInputOptions_t.svelte"
+	import PerFileInputOptions_sseof from "$lib/components/PerFileInputOptions_sseof.svelte"
   import { command } from "$lib/stores"
 	import { RangeSlider } from "@skeletonlabs/skeleton"
 
@@ -157,59 +158,8 @@
         <div class="input-group-shim my-2 mr-2">sec</div>
       </div>
     </div>
-
-    <div class="flex"><!-- start second relative to end-of-ofile (sseof) -->
-      <RangeSlider
-        name="range-slider-sseof"
-        class="flex-grow"
-        style="direction: rtl;"
-        bind:value={$command.inputs[input_index].ui.start_frame_eof}
-        on:change={() => {
-          command.update(($command) => {
-            $command.inputs[input_index].per_file_main_options.sseof = $command.inputs[input_index].ui.start_frame_eof / fps
-            return $command
-          })
-        }}
-        min={0}
-        max={frames}
-        step={1}
-        accent="accent-success-600 dark:accent-success-400"
-        title="send only the last {
-          $command.inputs[input_index].per_file_main_options.sseof.toFixed(2)
-          } seconds at the end of this input to be further processed"
-        disabled={
-          $command.inputs[input_index].ui.duration.sec === 0 && 
-          $command.inputs[input_index].ui.duration.min === 0
-        }
-      >
-        <div class="text-sm flex flex-row flex-wrap justify-between">
-          <div class="mx-2">start second relative to end-of-file (-sseof)</div>
-          <div class="mx-2">{start_second_eof.toFixed(2)} / {duration_seconds} sec</div>
-          <div class="mx-2">{Math.floor(start_second_eof / 60) + " min " + ((start_second_eof % 60)).toFixed(2) + " sec"}</div>
-          <!-- <div class="mx-2">frame -{start_frame_eof.toFixed(0)} / {frames.toFixed(0)}</div> -->
-        </div>
-      </RangeSlider>
-      <div class="input-group input-group-divider grid-cols-[auto_1fr_auto] max-w-[10rem] ml-2 my-2">
-        <input type="number" class="input max-w-[6rem]"
-          step="0.01"
-          title={
-            start_second_eof.toFixed(2) + " sec\n" +
-            Math.floor(start_second_eof / 60) + " min " + (start_second_eof % 60).toFixed(2) + " sec"
-          }
-          bind:value={$command.inputs[input_index].per_file_main_options.sseof}
-          on:change={()=>{
-            command.update(($command) => {
-              $command.inputs[input_index].ui.start_frame_eof = $command.inputs[input_index].per_file_main_options.sseof * fps
-              return $command
-            })
-          }}
-        >
-        <div class="input-group-shim my-2 mr-2">sec</div>
-      </div>
-    </div>
-
+    <PerFileInputOptions_sseof {input_index} {frames} {duration_seconds} {fps} {start_second_eof} />
     <PerFileInputOptions_t {input_index} {frames} {duration_seconds} {fps} />
-    
   </Expandable>
 </fieldset>
 
