1807b526b8ab878606075f7fb0f7952478621317
ViltsuV <9432001+ViltsuV@users.noreply.github.com>
2023-08-16 14:05:28 +0300

    fix ProjectListItem styling, use TechnologyChip instead

--- src/lib/components/ProjectsListItem.svelte
+++ src/lib/components/ProjectsListItem.svelte
@@ -1,63 +1,53 @@
 <script lang="ts">
 	import type { Project } from "$lib/types"
+  import TechnologyChip from "$lib/components/TechnologyChip.svelte"
 
   export let project: Project
 </script>
 
-<div class="projects-list-item flex flex-row">
-
+<a href='projects/{project.slug}'
+  class="projects-list-item
+    flex
+    flex-row
+    card
+    card-hover
+    bg-surface-200
+    dark:bg-surface-700
+    bg-opacity-90
+    rounded-md
+    mb-4
+    p-4
+    max-w-2xl
+    border-primary-900"
+>
   <img class="project-screenshot unselectable" src={project.screenshot.href} alt={project.screenshot.alt}>
 
-  <div class="project-info">
-    <a href='projects/{project.slug}' class="project-name">
+  <div class="project-info flex flex-col content-between justify-between pl-1">
+    <header class="project-name
+        card-header
+        text-tertiary-700
+        text-xl
+        dark:text-primary-500"
+    >
       {project.link.text || project.name ? project.link.text || project.name : '[project name]'}
-    </a>
-    <div class="project-description">{project.description ? project.description : '[project description]'}</div>
-    <div class="activity">
-      last activity: [todo: calculate last activity]
-    </div>
-    <div class="technologies">
-      tech stack / tools:
-      {#each project.technologies as technology}
-        <div class="technology technology-type-{technology.type}">
-          {technology.name}
-        </div>
-      {/each}
-    </div>
+    </header>
+    <section class="px-4">
+      <div class="project-description">{project.description ? project.description : '[project description]'}</div>
+    </section>
+    {#if project.technologies.length > 0}
+      <section class="technologies flex px-4">
+        {#each project.technologies as technology}
+          <TechnologyChip {technology} />
+        {/each}
+      </section>
+    {/if}
+    <footer class="card-footer">
+      <div class="activity">
+        last activity: [todo: calculate last activity]
+      </div>
+    </footer>
   </div>
-
-</div>
+</a>
 
 <style lang="postcss">
-  .projects-list-item {
-    @apply
-      rounded-md
-      mb-4
-      p-4
-      border-primary-900
-      bg-gray-500
-      bg-opacity-10
-  }
-  .project-info {
-    @apply
-      pl-4
-  }
-	.unselectable {
-		user-select: none
-	}
-  .technology-type-frontend {
-
-  }
-  .technology-type-backend {
-    
-  }
-  .technology-type-db {
-    
-  }
-  .technology-type-service {
-    
-  }
-  .technology-type-api {
-    
-  }
 </style>
